0451aa26f628aadc8775795b74f4c2fcd51fcab9
Sam Maxwell <sam@paybase.io>
2019-02-28 12:18:05 +0000

    test(shortest-path): sort double

--- src/index.js
+++ src/index.js
@@ -55,7 +55,7 @@ const shortestPath = (joins) => {
         .sort((a, b) => (console.log(a, b, a.length, b.length), a.length > b.length))
       : [];
   }
-  return (...x) => recurse(...x).shift();
+  return (...x) => recurse(...x).sort((a, b) => (console.log(a, b, a.length, b.length), a.length > b.length)).shift();
 };
 
 const createFinder = (edges) => ([ from, to ]) =>
